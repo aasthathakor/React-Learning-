a795c08783ec91c1c872bc7fff8f28e1bf4d50c0
Astha Thakur <128837575+aasthathakor@users.noreply.github.com>
2024-11-20 15:57:58 +0530

    Counter02 complete

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,25 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
-  let counter = 15
-  const addValue = () =>{
-    console.log("clicked", counter);
-    counter = counter + 1
+  const [counter, setCounter] = useState(15)
+
+  const addValue = () => {
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
   }
- 
 
-  return(
+  const removeValue = () => {
+    if (counter > 0) {
+      setCounter(prevCounter => prevCounter - 1)
+    }
+  }
+
+  return (
     <>
-    <h1>React</h1>
-    <h2>Counter Value : {counter} </h2>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
 
-    <button onClick={addValue}>Add Value</button>
-    <br></br>
-    <button>Remove Value</button>
-
+      <button onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>Remove value {counter}</button>
+      <p>Footer: {counter}</p>
     </>
   )
 }
 
-export default App
+export default App;
