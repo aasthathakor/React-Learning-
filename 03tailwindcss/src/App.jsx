5de6865406bec20beebd6a4eab4ea18bef2e1fee
Astha Thakur <128837575+aasthathakor@users.noreply.github.com>
2024-11-21 19:57:23 +0530

    TailwindCSS with Props

--- 03tailwindcss/src/App.jsx
+++ 03tailwindcss/src/App.jsx
@@ -1,31 +1,22 @@
+import { useState } from 'react';
 import './App.css';
+import Card from './Components/Card';
 
 function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "React",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
 
   return (
     <>
     <h1 class = 'text-center bg-green-300 text-black font-bold p-4 rounded-full'>Tailwind CSS Test</h1>
-    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
-  <img class="w-24 h-24 rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512" />
-  <div class="pt-6 text-center space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+    <Card username="react code" btnText="Click me" />
+    <Card username="react code" btnText="visit me" />
     </>
+    
   );
   
 }
